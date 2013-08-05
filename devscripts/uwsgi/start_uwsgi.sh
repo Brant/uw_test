33089aa3efa84593ab70d54b4c34e2a7cc70a17b
Tim White <tim@cyface.com>
2013-08-05 16:31:53 -0600

    Couple minor tweaks.

--- devscripts/uwsgi/start_uwsgi.sh
+++ devscripts/uwsgi/start_uwsgi.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #Sets the environment to point at the correct settings file
 # Run this from the uw_test dir (outer)
+# So you should be running ./devscripts/uwsgi/start_uwsgi.sh
+
 . .env/bin/activate
 uwsgi --ini devscripts/uwsgi/uwsgi_config.ini
